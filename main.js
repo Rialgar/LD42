8847b065149db624498d7e9ccc32377853e06089
Benjamin Siegmund <rialgar42@gmail.com>
2018-08-11 14:35:52 +0200

    scores and buyers leaving

--- main.js
+++ main.js
@@ -9,16 +9,25 @@ Array.prototype.shuffle = function () {
 }
 
 window.addEventListener('load', () => {
-    const gamestate = {};
+    const gamestate = {
+        dom: {
+            store: document.getElementById('store'),
+            buyers: document.getElementById('buyers'),
+            sellers: document.getElementById('sellers'),
+            scores: document.getElementById('scores'),
+            lives: document.getElementById('lives')
+        }
+    }
 
     const MAX_SELLERS = 6;
     const ACTIVE_SELLERS = 2;
     const SELLER_INTERVAL = 3 * 1000;
-    const MAX_BUYERS = 5;
-    const ACTIVE_BUYERS = 1;
+    const MAX_BUYERS = 6;
+    const ACTIVE_BUYERS = 2;
     const BUYER_INTERVAL = 3 * 1000;
     const MAX_STORE = 10;
     const LEVEL_COUNT = 15;
+    const BUYER_TIME = 30 * 1000;
 
     const ITEMS = [
         { name: 'Apple' },
@@ -38,26 +47,6 @@ window.addEventListener('load', () => {
         { name: 'Melon' },
         { name: 'Pumpkin' }
     ].shuffle();
-
-    gamestate.buyers = [];
-    gamestate.store = [];
-    gamestate.sellers = [];
-    gamestate.itemTypes = 3;
-    gamestate.itemsBought = 0;
-    gamestate.itemsSold = 0;
-
-    gamestate.dom = {
-        store: document.getElementById('store'),
-        buyers: document.getElementById('buyers'),
-        sellers: document.getElementById('sellers')
-    }
-
-    gamestate.times = {
-        sellerCreated: 0,
-        buyerCreated: 0,
-        bought: 0,
-        sold: 0
-    }
 
     const getItem = () => ITEMS[Math.floor(Math.random() * gamestate.itemTypes)];
 
@@ -105,8 +94,6 @@ window.addEventListener('load', () => {
 
                     gamestate.itemsSold++;
                     gamestate.times.sold++;
-
-                    gamestate.itemTypes = 3 + Math.floor(gamestate.itemsSold / LEVEL_COUNT);
                 }
             }
 
@@ -116,7 +103,6 @@ window.addEventListener('load', () => {
     for (let i = 0; i < MAX_STORE; i++) {
         const div = document.createElement('div');
         div.classList.add('item');
-        div.classList.add('empty');
         div.addEventListener('click', (index => () => sellItem(index))(i))
         gamestate.dom.store.appendChild(div);
     }
@@ -146,17 +132,13 @@ window.addEventListener('load', () => {
             gamestate.times.sellerCreated = Date.now();
         }
     }
-
-    makeSeller();
-    makeSeller();
-    makeSeller();
-
     const makeBuyer = () => {
         if (gamestate.buyers.length < MAX_BUYERS) {
             const buyer = {
                 item: getItem(),
                 dom: document.createElement('div'),
-                created: Date.now()
+                created: Date.now(),
+                left: BUYER_TIME
             }
 
             buyer.dom.classList.add('item');
@@ -168,15 +150,62 @@ window.addEventListener('load', () => {
         }
     }
 
-    makeBuyer();
-    makeBuyer();
-    makeBuyer();
+    const updateBuyers = now => {
+        gamestate.buyers.forEach(buyer => {
+            buyer.left = BUYER_TIME - (now - buyer.created);
+            const percentage = buyer.left / BUYER_TIME * 100;
+            buyer.dom.style.background = `linear-gradient(white, white ${percentage}%, red ${percentage}%, red)`;
+        });
+
+        const toRemove = gamestate.buyers.filter(buyer => buyer.left < 0);
+        gamestate.lives -= toRemove.length;
+        toRemove.forEach(buyer => buyer.dom.parentElement.removeChild(buyer.dom));
+
+        gamestate.buyers = gamestate.buyers.filter(buyer => buyer.left >= 0);
+    }
+
+    const init = () => {
+        gamestate.buyers = [];
+        gamestate.store = [];
+        gamestate.sellers = [];
+        gamestate.itemTypes = 3;
+        gamestate.itemsBought = 0;
+        gamestate.itemsSold = 0;
+        gamestate.lives = 5;
+
+        gamestate.times = {
+            sellerCreated: 0,
+            buyerCreated: 0,
+            bought: 0,
+            sold: 0
+        }
+
+        while (gamestate.dom.buyers.childElementCount > 0) {
+            gamestate.dom.buyers.removeChild(gamestate.dom.buyers.firstChild);
+        }
+        while (gamestate.dom.sellers.childElementCount > 0) {
+            gamestate.dom.buyers.removeChild(gamestate.dom.buyers.firstChild);
+        }
+        Array.prototype.forEach.call(gamestate.dom.store.children, el => {
+            el.classList.add('empty');
+            el.textContent = '';
+        });
+
+        for (let i = 0; i < 3; i++) {
+            makeSeller();
+            makeBuyer();
+        }
+    }
+
+    init();
 
     let last = Date.now();
     const step = () => {
         const now = Date.now();
         const delta = now - last;
         last = now;
+
+        gamestate.itemTypes = 3 + Math.floor(gamestate.itemsSold / LEVEL_COUNT);
 
         if (now - gamestate.times.sellerCreated > SELLER_INTERVAL) {
             makeSeller();
@@ -185,6 +214,11 @@ window.addEventListener('load', () => {
         if (now - gamestate.times.buyerCreated > BUYER_INTERVAL) {
             makeBuyer();
         }
+
+        updateBuyers(now);
+
+        gamestate.dom.scores.textContent = gamestate.itemsSold;
+        gamestate.dom.lives.textContent = gamestate.lives;
 
         window.requestAnimationFrame(step);
     }
